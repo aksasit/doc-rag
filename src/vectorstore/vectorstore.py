9d93aa4068eda1983adcb41f4527340f273a36a6
asit <sahooasit621@gmail.com>
2026-03-15 12:23:54 +0530

    vectorstore and state is defined

---
+++ src/vectorstore/vectorstore.py
@@ -0,0 +1,51 @@
+"""Vector store for document embedding and retrieval"""
+
+from typing import List
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain.schema import Document
+
+class VectorStore:
+    """Manage vector store application"""
+    def __init__(self):
+        self.embedding=OpenAIEmbeddings()
+        self.vectorstore=None
+        self.retriever=None
+        
+    def create_retriever(self, documents: List[Document]):
+        """
+        Create a vector store from documents
+
+        Args:
+            documents: List of documents to embed
+        """
+        
+        self.vectorstore = FAISS.from_documents(documents, self.embedding)
+        self.retriever = self.vectorstore.as_retriever()
+    
+    def get_retriever(self):
+        """
+        Get the retriever insatance
+        
+        Returns:
+            Retriver instance
+        """
+        if self.retriever is None:
+            raise ValueError("Vector store has not been intiialized. Call create_vectorstore first.")
+        return self.retriever
+    
+    def retrieve(self, query: str, k: int = 4) -> List[Document]:
+        """
+        Retrieve relevant documents from query
+        
+        Args:
+            query (str): search string
+            k (int, optional): Number of documents to retrieve. Defaults to 4.
+
+        Returns:
+            List[Document]: List of relevant documents
+        """
+        if self.retriever is None:
+            raise ValueError("Vector store not initialized. Call create_vectorstore first")
+        return self.retriever.invoke(query)
+    
